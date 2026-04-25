577c740cc96cc7946c1fb148a2fa91eedc804a7a
mir-src <264200162+mir-src@users.noreply.github.com>
2026-04-25 14:55:45 +0300

    feat: AddToInventory method and Helpers for reading int and string safely

---
+++ InventorySystem/Helpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventorySystem
+{
+    internal class Helpers
+    {
+        public static int ReadInt()
+        {
+            while (true)
+            {
+                var intVar = Console.ReadLine();
+                if (int.TryParse(intVar, out var intVarOut)) return intVarOut;
+                Console.WriteLine("Invalid integer.");
+            }
+        }
+        public static string ReadString()
+        {
+            while (true) {
+                var stringVar = Console.ReadLine();
+                if (!string.IsNullOrEmpty(stringVar))
+                {
+                    return stringVar;
+                }
+                Console.WriteLine("Invalid string.");
+            }
+        }
+    }
+}
--- InventorySystem/Program.cs
+++ InventorySystem/Program.cs
@@ -6,19 +6,26 @@ class Program
 {
     class Inventory
     {
-        public Dictionary<string,int> Storage { get; set; }
+        public Dictionary<string,int> Storage { get; private set; }
 
         public Inventory()
         {
             Storage = new Dictionary<string, int>();
         }
+        public void AddToInventory(string product, int amount)
+        {
+            Storage[product] = amount;
+        }
+
     }
     public static void Main(string[] args)
     {
-        Inventory testObject = new Inventory();
-        testObject.Storage["Cake"] = 100;
-        testObject.Storage["Pepsi"] = 210;
-        foreach (KeyValuePair<string,int> item in testObject.Storage) {
+        Inventory objectInventory = new Inventory();
+        int mAmount = Helpers.ReadInt();
+        string mProduct = Helpers.ReadString();
+        objectInventory.AddToInventory(mProduct,mAmount);
+        foreach (KeyValuePair<string,int> item in objectInventory.Storage)
+        {
             Console.WriteLine($"Key: {item.Key} | Value: {item.Value}");
         }
     }
