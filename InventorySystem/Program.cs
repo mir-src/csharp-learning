85b0491a9e49693e967a76e33fb4f2280c3ee73b
mir-src <264200162+mir-src@users.noreply.github.com>
2026-04-25 14:32:26 +0300

    feat: Inventory class constructor

--- InventorySystem/Program.cs
+++ InventorySystem/Program.cs
@@ -4,8 +4,22 @@ namespace InventorySystem;
 
 class Program
 {
+    class Inventory
+    {
+        public Dictionary<string,int> Storage { get; set; }
+
+        public Inventory()
+        {
+            Storage = new Dictionary<string, int>();
+        }
+    }
     public static void Main(string[] args)
     {
-
+        Inventory testObject = new Inventory();
+        testObject.Storage["Cake"] = 100;
+        testObject.Storage["Pepsi"] = 210;
+        foreach (KeyValuePair<string,int> item in testObject.Storage) {
+            Console.WriteLine($"Key: {item.Key} | Value: {item.Value}");
+        }
     }
 }
