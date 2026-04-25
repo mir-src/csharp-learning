58dc5d05d18331e782af7b016920edf2e84360e3
mir-src <264200162+mir-src@users.noreply.github.com>
2026-04-25 16:15:12 +0300

    feat: Working number guessing game

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -16,11 +16,43 @@ class Program
             if (!int.TryParse(Console.ReadLine(), out var choice))
             {
                 Console.WriteLine("Invalid number.");
+                continue;
             }
 
             switch (choice)
             {
                 case 1:
+                    Random randomNumber = new Random();
+                    int randomNr = randomNumber.Next(1, 11);
+                    while (true)
+                    {
+                        Console.Clear();
+                        Console.Write("Enter number: ");
+                        if (!int.TryParse(Console.ReadLine(), out var user))
+                        {
+                            Console.WriteLine("Invalid number.");
+                            continue;
+                        }
+
+                        if (user == randomNr)
+                        {
+                            Console.WriteLine("You guessed the number correctly.");
+                            break;
+                        }
+                        else
+                        {
+                            if (user < randomNr) {
+                                Console.WriteLine("The number is HIGHER.");
+                            }
+                            if (user > randomNr)
+                            {
+                                Console.WriteLine("The number is LOWER");
+                            }
+                        }
+                        Console.WriteLine("\nTry again. Press any button...");
+                        Console.ReadKey();
+                    }
+
                     break;
                 case 2:
                     return;
