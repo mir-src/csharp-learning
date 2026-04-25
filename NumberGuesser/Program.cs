18055898c297f3d13e497822deb25523604f6526
mir-src <264200162+mir-src@users.noreply.github.com>
2026-04-25 15:57:54 +0300

    feat: Game system

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -6,6 +6,27 @@ class Program
 {
     public static void Main(string[] args)
     {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("--- Number Guesser ---");
+            Console.WriteLine("1. Play game");
+            Console.WriteLine("2. Quit");
+            
+            if (!int.TryParse(Console.ReadLine(), out var choice))
+            {
+                Console.WriteLine("Invalid number.");
+            }
 
+            switch (choice)
+            {
+                case 1:
+                    break;
+                case 2:
+                    return;
+            }
+            Console.WriteLine("\nType any key to return to Menu...");
+            Console.ReadKey();
+        }
     }
 } 
