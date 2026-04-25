beb49e2cdb563d2603f78d78027e7feeab99ca11
mir-src <264200162+mir-src@users.noreply.github.com>
2026-04-25 08:48:04 +0300

    feat: Create account (CRUD)

--- BankAccountSystem/Program.cs
+++ BankAccountSystem/Program.cs
@@ -3,6 +3,15 @@ namespace BankAccountSystem;
 
 class Program
 {
+    class Helpers
+    {
+        public static int ReadInt()
+        {
+            var input = Console.ReadLine();
+            if (int.TryParse(input, out var output)) return output;
+            else throw new ArgumentException("Invalid int");
+        }  
+    }
     class Bank
     {
         public string Name { get; private set; }
@@ -62,56 +71,6 @@ class Program
     }
     public static void Main(string[] args)
     {
-        Bank account = new Bank("Cornelia", 100);
-        Bank secondAccount = new Bank("Iulian", 100);
-        Console.WriteLine(account.Name);
-        Console.WriteLine(account.Balance);
-        Console.WriteLine(account.Frozen);
-
-        Console.WriteLine("---------");
-        account.AddMoney(200);
-        Console.WriteLine(account.Balance);
-
-        account.SwitchState();
-        account.SwitchState();
-
-        try
-        {
-            account.Withdraw(600);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-        Console.WriteLine(account.Balance);
-
-        Console.WriteLine($"Name: {account.Name} | {account.Balance}");
-        Console.WriteLine($"Name: {secondAccount.Name} | {secondAccount.Balance}");
-
-        try
-        {
-            account.Transfer(200);
-            account.Withdraw(200);
-            secondAccount.AddMoney(200);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-
-        Console.WriteLine($"Name: {account.Name} | {account.Balance}");
-        Console.WriteLine($"Name: {secondAccount.Name} | {secondAccount.Balance}");
-
-        
-
-
-        
-
-
-
-
-        
-        /*
         Console.Clear();
         Console.WriteLine("---Bank System---");
         Console.WriteLine("1. Create Account");
@@ -122,18 +81,63 @@ class Program
         Console.WriteLine("6. Freeze or Unfreeze Account");
         Console.WriteLine("7. Quit Menu");
 
+        List<Bank> bankList = new List<Bank>();
+
+        while (true) { 
+            string choice = Console.ReadLine()?.Trim();
+            if (string.IsNullOrEmpty(choice))
+            {
+                Console.WriteLine("Enter a valid option.");
+                continue;
+            }
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Enter name of account: ");
+                    string c1Name = Console.ReadLine();
+                    Console.WriteLine();
+
+                    if (string.IsNullOrEmpty(c1Name))
+                    {
+                        Console.WriteLine("Invalid account name.");
+                        break;
+                    }
+
+                    Console.Write("Enter current Balance: ");
+                    int c1Input = Helpers.ReadInt();
+                    try
+                    {
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        break;
+                    }
+                    int c1Balance = c1Input;
+                    Console.WriteLine();
+
+                    bankList.Add(new Bank(c1Name,c1Balance));
+                    Console.WriteLine($"Bank Account: {c1Name} | Created");
+
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    break;
+                case "7":
+                    return;
+            }
+            break;
+        }
+
         Console.WriteLine("\n Type any key to go back to Menu...");
         Console.ReadKey();
-        */
+        
     }
 }
-
-/// class BankAccounts  
-/// account name (string)  
-/// account balance (int)  
-/// account frozen (bool) 
-
-/// Add money - Withdraw money - Transfer money between two accounts 
-/// Remove account - Create account - Toggle frozen account switch 
-
-/// Display menu and options
