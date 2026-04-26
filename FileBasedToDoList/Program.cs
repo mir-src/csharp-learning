645539df43f899e438667cfec285421478dfd620
mir-src <264200162+mir-src@users.noreply.github.com>
2026-04-26 18:03:24 +0300

    feat: Add task to list

--- FileBasedToDoList/Program.cs
+++ FileBasedToDoList/Program.cs
@@ -8,17 +8,51 @@ class Program
     {
         string filePath = "tasks.json";
         List<Task> tasks = new List<Task>();
+        int id = 0;
 
         while (true)
         {
             Console.Clear();
-            Console.WriteLine("1. Add task to list");
-            Console.WriteLine("2. Remove task from list");
+            Console.WriteLine("1. Add Task to list");
+            Console.WriteLine("2. Remove Task from list");
             Console.WriteLine("3. Toggle Task Completion");
             Console.WriteLine("4. Display Tasks");
-            Console.WriteLine("5. Delete All tasks");
+            Console.WriteLine("5. Delete All Tasks");
             Console.WriteLine("6. Mark All Tasks as Complete/Incomplete");
             Console.WriteLine("7. Quit Menu");
+
+            Console.Write("\nSelect your option: ");
+            string? choice = Console.ReadLine();
+            if (string.IsNullOrEmpty(choice)) continue;
+
+            tasks = Storage.Load(filePath);
+
+            switch (choice)
+            {
+                case "1":
+                    id++;
+                    string? c1Name = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(c1Name)) break;
+                    tasks.Add(new Task(id,c1Name));
+
+                    Storage.Save(filePath, tasks);
+
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    break;
+                case "7":
+                    return;
+            }
+
 
             Console.WriteLine("\n Type any key to return to Menu...");
             Console.ReadKey();
