6d4eb8a708da27a3790fea00531f7c145bdd9ea0
mir-src <264200162+mir-src@users.noreply.github.com>
2026-05-11 15:17:36 +0300

    feat: Flashcard Class

--- ConsoleAnki/Program.cs
+++ ConsoleAnki/Program.cs
@@ -1,1 +1,30 @@
-﻿// Write simple flashcard app without database
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAnki;
+
+class Program
+{
+    class Flashcard
+    {
+        public string Front { get; set; }
+        public string Back { get; set; }
+        public Flashcard(string front, string back)
+        {
+            Front = front;
+            Back = back;
+        }
+    }
+    public static void Main(string[] args)
+    {
+        List<Flashcard> flashcards = new List<Flashcard>();
+        flashcards.Add(new Flashcard("The capital of Japan is...", "Tokyo"));
+        flashcards.Add(new Flashcard("The capital of France is...", "Paris"));
+        foreach (Flashcard card in flashcards)
+        {
+            Console.WriteLine(card.Front);
+            Console.WriteLine(card.Back);
+        }
+    }
+}
+// Front, Back, Pass / Fail - Simple answer system
