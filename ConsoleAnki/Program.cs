36fc3e94dafee04dfb5e844e31f09bb8d2844669
mir-src <264200162+mir-src@users.noreply.github.com>
2026-05-12 08:46:35 +0300

    feat: Add flashcard flow implemented

--- ConsoleAnki/Program.cs
+++ ConsoleAnki/Program.cs
@@ -21,18 +21,58 @@ class Program
     {
         public static void Start()
         {
+            List<Flashcard> cards = new List<Flashcard>();
+            int id = 1; 
             while (true)
             {
+                Console.Clear();
                 Console.WriteLine("--- ANKI CONSOLE ---");
                 Console.WriteLine("1. Review Flashcards");
                 Console.WriteLine("2. Show Flashcards");
                 Console.WriteLine("3. Delete Flashcards");
                 Console.WriteLine("4. Add Flashcards");
                 Console.WriteLine("5. Quit");
-                List<Flashcard> cards = new List<Flashcard>();
-                cards.Add(new Flashcard(1, "Capital of Japan...", "Tokyo"));
-                Game.ShowCards(cards);   
-                return;
+
+                string choice = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(choice))
+                {
+                    Console.WriteLine("Invalid choice");
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        Game.ShowCards(cards);
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        Console.Write("Front: ");
+                        string c4Front = Console.ReadLine();
+                        Console.Write("\n");
+                        if (string.IsNullOrWhiteSpace(c4Front))
+                        {
+                            Console.WriteLine("Invalid Front of the card");
+                        }
+                        Console.Write("Back: ");
+                        string c4Back = Console.ReadLine();
+                        Console.Write("\n");
+                        if (string.IsNullOrWhiteSpace(c4Front))
+                        {
+                            Console.WriteLine("Invalid Back of the card");
+                        }
+                        cards.Add(new Flashcard(id, c4Front, c4Back));
+                        id++;
+                        Console.WriteLine("Card added succesfully!");
+                        break;
+                    case "5":
+                        return;
+                }
+                Console.WriteLine("\n Type any key to return to menu...");
+                Console.ReadKey();
             }
         }
         public static void ShowCards(List<Flashcard> cards)
@@ -48,4 +88,4 @@ class Program
         Game.Start();
     }
 }
-// TODO: Add Flashcard to Deck Flow 
+// TODO: Add delete card from deck flow  
