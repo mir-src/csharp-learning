fe31232ae607f5f8e999845c461595c02e510bc8
mir-src <264200162+mir-src@users.noreply.github.com>
2026-05-15 07:27:20 +0300

    Implemented add habit and get habit method

--- MVPHabitTracker/Program.cs
+++ MVPHabitTracker/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using MVPHabitTracker.Models;
+using MVPHabitTracker.Services;
 
 namespace MVPHabitTracker;
 
@@ -7,5 +8,17 @@ class Program
 {
     public static void Main(string[] args)
     {
+        DateOnly today = DateOnly.FromDateTime(DateTime.Today);  
+        YesNoHabit habitOne = new YesNoHabit("Coding",today); 
+        YesNoHabit habitTwo = new YesNoHabit("Japanese",today); 
+
+        HabitService habits = new HabitService();
+        habits.AddHabit(habitOne);
+        habits.AddHabit(habitTwo);
+        List<Habit> habitList = habits.GetHabits();
+        foreach (Habit habit in habitList)
+        {
+            Console.WriteLine($"{habit.Name} | {habit.CreatedAt}");
+        }
     }
 }
--- MVPHabitTracker/Services/HabitService.cs
+++ MVPHabitTracker/Services/HabitService.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MVPHabitTracker.Models;
 
 namespace MVPHabitTracker.Services
 {
     internal class HabitService
     {
+        List<Habit> Habits;
+        public HabitService()
+        {
+            Habits = new List<Habit>();
+        }
+        public void AddHabit(Habit habit)
+        {
+            Habits.Add(habit);
+        }
+        public List<Habit> GetHabits()
+        {
+            return Habits;
+        }
     }
 }
